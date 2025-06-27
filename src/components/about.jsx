1c45cbb84b0197efacccf40c20619a2516f19e4b
Preetam <120736203+hacknotch@users.noreply.github.com>
2025-06-27 14:03:31 +0530

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -45,7 +45,7 @@ export default function About() {
 
           {/* Resume Button */}
           <a
-          href="https://tinyurl.com/resume-preetam"
+          href="https://tinyurl.com/preetam-resume"
           className="inline-block bg-gradient-to-r from-teal-500 to-cyan-500 hover:from-cyan-500 hover:to-teal-500 text-white font-bold py-3 px-6 rounded-xl shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-cyan-400/50"
         >
           🚀 RESUME 
